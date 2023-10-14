60f0bf7f740969a0b94a34942f607d3ad44aba0d
nuzulul <nuzulz@gmail.com>
2023-10-14 23:58:28 +0700

    production infopalestina2

--- src/index.js
+++ src/index.js
@@ -30,6 +30,7 @@ const handleGet = async (request, env, id) => {
   if (id) {
     switch(id) {
       case "infopalestina": return await handleInfopalestina(request, env)
+      case "infopalestina2": return await handleInfopalestina2(request, env)
     }
   }
   else {
@@ -232,6 +233,124 @@ const handleInfopalestina = async (request, env) => {
     //if (data[i].id == 8080) {
       await env.DB.put("infopalestina",data[i].id)
       let title = data[i].title.replace("#SahabatPalestina_ID","")
+      title = decodeEntities(title)
+      //console.log(title)
+      if(data[i].photo) {
+        const response = await sendtelegram("photo",title,data[i].photo)
+        data[i].response = response
+      }
+      else if(data[i].video) {
+        const response = await sendtelegram("video",title,data[i].video)
+        data[i].response = response
+      }
+      else {
+        const response = await sendtelegram("text",title,"")
+        data[i].response = response
+      }      
+      output = JSON.stringify(data[i], null, 2)
+      break
+    }
+  }
+  
+  //const res = json
+  const res = output
+  return new Response(res, {
+    headers: {
+      "content-type": "application/json;charset=UTF-8"
+      //"content-type": "text/html;charset=UTF-8"
+    }
+  });  
+}
+
+const handleInfopalestina2 = async (request, env) => {
+
+  addEventListener("fetch", event => {
+    event.respondWith(handleRequest(event.request))
+  });
+
+  async function handleRequest(request) {
+    var values = [];
+    const url = "https://t.me/s/infopalestineterkini"
+    var response = await fetch(url);
+    function addToLast(attr, text) {
+      var lastIndex = values.length - 1;
+      if (lastIndex < 0) {
+        // this shouldn't happen, since there should always have been
+        // an object created by the parent [data-code] div
+        return;
+      }
+      // need to add them to the previous value, just in case if there
+      // are multiple text chunks
+      values[lastIndex][attr] = (values[lastIndex][attr] || '') + text;
+    }
+    await new HTMLRewriter()
+      .on(".tgme_widget_message_wrap", { 
+        element(element) { 
+          values.push({
+            container: element.getAttribute("class")
+          });
+        }
+      })
+      .on(".tgme_widget_message_wrap .tgme_widget_message", {
+        element(element) {
+          addToLast('code', element.getAttribute("data-post"));
+          const code = element.getAttribute("data-post")
+          const arr = code.split("/")
+          const id = arr[1]
+          addToLast('id',id)
+        }
+      })
+      .on(".tgme_widget_message_wrap .tgme_widget_message .tgme_widget_message_bubble .tgme_widget_message_text", {
+        text(text) {
+          addToLast('title', text.text);
+        }
+      })
+      .on(".tgme_widget_message_wrap .tgme_widget_message .tgme_widget_message_bubble .tgme_widget_message_photo_wrap", {
+        element(element) {
+
+          const regex = /background-image:url\(["']?([^"']*)["']?\)/gm;
+          const str = element.getAttribute("style");
+          let m;
+
+          while ((m = regex.exec(str)) !== null) {
+              // This is necessary to avoid infinite loops with zero-width matches
+              if (m.index === regex.lastIndex) {
+                  regex.lastIndex++;
+              }
+              
+              // The result can be accessed through the `m`-variable.
+              m.forEach((match, groupIndex) => {
+                  //console.log(`Found match, group ${groupIndex}: ${match}`);
+                  if (groupIndex == 1) addToLast('photo', match);
+              });
+          }
+          
+          
+        }
+      })
+      .on(".tgme_widget_message_wrap .tgme_widget_message .tgme_widget_message_bubble .tgme_widget_message_video_player .tgme_widget_message_video_wrap video", {
+        element(element) {
+          addToLast('video', element.getAttribute("src"));
+        }
+      })
+      .transform(response).arrayBuffer();
+    
+    return values
+  }
+  
+  const data = await handleRequest(request)
+  const json = JSON.stringify(data, null, 2)
+  //await env.DB.put("infopalestina2",2)
+  const lastcode = await env.DB.get("infopalestina2")
+  //console.log(lastcode)
+  let output = '{"status":"ok"}'
+  
+  for (let i = 0; i < data.length - 1;i++) {
+    //console.log(data[i])
+    if (data[i].id > lastcode) {
+    //if (data[i].id == 8080) {
+      await env.DB.put("infopalestina2",data[i].id)
+      let title = data[i].title.replace("##########","")
       title = decodeEntities(title)
       //console.log(title)
       if(data[i].photo) {
