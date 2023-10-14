631f3a27ea68c591c051dbcef69ff80700d51dd4
nuzulul <nuzulz@gmail.com>
2023-10-14 22:14:28 +0700

    production=infopalestina

--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+let myenv
+
 export default {
 	async fetch(request, env, ctx) {
 		return await handleRequest(request, env, ctx)
@@ -18,6 +20,7 @@ async function handleRequest(request, env, ctx) {
     const { method, url } = request
     const { searchParams } = new URL(url)
     const id = searchParams.get('id')
+    myenv = env
     switch(method) {
         case "GET": return await handleGet(request, env, id)
     }
@@ -34,6 +37,111 @@ const handleGet = async (request, env, id) => {
   }
 }
 
+async function sendtelegram(format,caption,src) {
+  
+  const TELEGRAM_BOT_TOKEN = myenv.API_TOKEN_TELEGRAM_NUZULUL_INFOBSMIBOT
+  const chatId = myenv.TELEGRAM_CHATID_INFOPALESTINA
+  //const chatId = "@bsmi_tv"
+  
+  if(format == "photo") {
+      const apiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`;
+      const params = {
+        chat_id: chatId,
+        photo: src,
+        caption: caption,
+      };
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+
+        if (response.ok) {
+          return 'Message sent successfully!'
+        } else {
+          return 'Failed to send message.'
+        }
+      } catch (error) {
+        console.error(error);
+        return 'Error occurred while sending the message.'
+      }
+  }
+  else if(format == "video") {
+      const apiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendVideo`;
+      const params = {
+        chat_id: chatId,
+        video: src,
+        caption: caption,
+      };
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+
+        if (response.ok) {
+          return 'Message sent successfully!'
+        } else {
+          return 'Failed to send message.'
+        }
+      } catch (error) {
+        console.error(error);
+        return 'Error occurred while sending the message.'
+      }
+  }
+  else if(format == "text"){
+      const apiUrl = `https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage`;
+      const params = {
+        chat_id: chatId,
+        text: caption,
+      };
+
+      try {
+        const response = await fetch(apiUrl, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(params),
+        });
+
+        if (response.ok) {
+          return 'Message sent successfully!'
+        } else {
+          return 'Failed to send message.'
+        }
+      } catch (error) {
+        console.error(error);
+        return 'Error occurred while sending the message.'
+      }
+  }
+}
+
+function decodeEntities(encodedString) {
+    var translate_re = /&(nbsp|amp|quot|lt|gt);/g;
+    var translate = {
+        "nbsp":" ",
+        "amp" : "&",
+        "quot": "\"",
+        "lt"  : "<",
+        "gt"  : ">"
+    };
+    return encodedString.replace(translate_re, function(match, entity) {
+        return translate[entity];
+    }).replace(/&#(\d+);/gi, function(match, numStr) {
+        var num = parseInt(numStr, 10);
+        return String.fromCharCode(num);
+    });
+}
+
 
 const handleInfopalestina = async (request, env) => {
 
@@ -43,7 +151,7 @@ const handleInfopalestina = async (request, env) => {
 
   async function handleRequest(request) {
     var values = [];
-    var url = "https://t.me/s/SahabatPalestinaID"
+    const url = "https://t.me/s/SahabatPalestinaID"
     var response = await fetch(url);
     function addToLast(attr, text) {
       var lastIndex = values.length - 1;
@@ -62,14 +170,15 @@ const handleInfopalestina = async (request, env) => {
           values.push({
             container: element.getAttribute("class")
           });
-        },
-        text(text) {
-          addToLast('body', text.text);
         }
       })
       .on(".tgme_widget_message_wrap .tgme_widget_message", {
         element(element) {
           addToLast('code', element.getAttribute("data-post"));
+          const code = element.getAttribute("data-post")
+          const arr = code.split("/")
+          const id = arr[1]
+          addToLast('id',id)
         }
       })
       .on(".tgme_widget_message_wrap .tgme_widget_message .tgme_widget_message_bubble .tgme_widget_message_text", {
@@ -106,13 +215,49 @@ const handleInfopalestina = async (request, env) => {
         }
       })
       .transform(response).arrayBuffer();
-    const json = JSON.stringify(values, null, 2)
-    return json
+    
+    return values
   }
-  const json = await handleRequest(request)
-  return new Response(json, {
+  
+  const data = await handleRequest(request)
+  const json = JSON.stringify(data, null, 2)
+  //await env.DB.put("infopalestina",2)
+  const lastcode = await env.DB.get("infopalestina")
+  //console.log(lastcode)
+  let output = '{"status":"ok"}'
+  
+  for (let i = 0; i < data.length - 1;i++) {
+    //console.log(data[i])
+    if (data[i].id > lastcode) {
+    //if (data[i].id == 8080) {
+      await env.DB.put("infopalestina",data[i].id)
+      let title = data[i].title.replace("#SahabatPalestina_ID","")
+      title = decodeEntities(title)
+      //console.log(title)
+      if(data[i].photo) {
+        const response = await sendtelegram("photo",title,data[i].photo)
+        data[i].response = response
+      }
+      else if(data[i].video) {
+        const response = await sendtelegram("video",title,data[i].video)
+        data[i].response = response
+      }
+      else {
+        const response = await sendtelegram("text",title,"")
+        data[i].response = response
+      }      
+      output = JSON.stringify(data[i], null, 2)
+      break
+    }
+  }
+  
+  //const res = json
+  const res = output
+  return new Response(res, {
     headers: {
       "content-type": "application/json;charset=UTF-8"
+      //"content-type": "text/html;charset=UTF-8"
     }
   });  
 }
+
